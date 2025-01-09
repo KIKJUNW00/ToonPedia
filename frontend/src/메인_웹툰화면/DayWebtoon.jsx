41b524b63e7d5de2b34889a8aa10d814750efa6f
lenoau <ysj1423y@naver.com>
2025-01-09 17:47:38 +0900

    마이페이지_완

--- frontend/src/메인_웹툰화면/DayWebtoon.jsx
+++ frontend/src/메인_웹툰화면/DayWebtoon.jsx
@@ -61,6 +61,15 @@ export default function Day() {
     }
   };
 
+  const handleLike = (webtoon) => {
+    const isLiked = likedWebtoons.some((item) => item.code === webtoon.id.trim());
+    if (isLiked) {
+      setLikedWebtoons(likedWebtoons.filter((item) => item.code !== webtoon.id.trim()));
+    } else {
+      setLikedWebtoons([...likedWebtoons, { code: webtoon.id.trim(), ...webtoon }]);
+    }
+  };
+
   useEffect(() => {
     fetchMy(); // 로그인된 사용자의 좋아요 웹툰을 가져옴
     fetchAllWebtoons(); // 모든 무료 웹툰 데이터를 가져옴
@@ -82,7 +91,7 @@ export default function Day() {
               <ul>
                 {webtoonData[key].map((webtoon) => (
                   <li key={webtoon.id}>
-                    <EveryDayToonCard webtoons={[webtoon]} likedWebtoonsList={likedWebtoons} />
+                    <EveryDayToonCard webtoons={[webtoon]} likedWebtoonsList={likedWebtoons} handleLike={handleLike} />
                   </li>
                 ))}
               </ul>
